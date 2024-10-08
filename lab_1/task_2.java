55da1eee7fc6a56e675b719d516762940e9142fd
F23ARI42 <F23ARI42>
2024-10-08 12:13:22 -0700

    message

---
+++ lab_1/task_2.java
@@ -0,0 +1,11 @@
+package lab_1;
+
+public class task_2 {
+    public static void main(String[] args) {
+        int array[]={5 ,15 ,25 ,35 ,45 ,55};
+        for(int i=0;i<array.length-1;i++){
+            System.out.println("Element at index:"+i+":"+array[i]);
+        }
+    }
+}
+}
