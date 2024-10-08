43e04a0fab2e3167002edefcd1b4ff9c44644cb8
F23ARI42 <F23ARI42>
2024-10-08 12:11:23 -0700

    message

---
+++ lab_1/task_1.java
@@ -0,0 +1,10 @@
+package lab_1;
+
+public class task_1 {
+    public static void main(String[] args) {
+        int array[]={5, 15, 25, 35, 45,55};
+        for (int i=0;i<array.length;i++){
+            System.out.println(array[i]);
+        }
+    }
+}
